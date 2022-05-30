ba2b54d8581ed8fd16e512b3ea4f4ec6e3f4d8b2
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-05-31 00:30:34 +0900

    IndexedDBにitemsの内容を保存する処理を実装

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <button @click="addItem">Add Item</button>
+    <button @click="addItem">Item追加</button>
+    <button @click="saveToIndexedDB">IndexedDBに保存</button>
     <button @click="postDirectly">itemsの要素を直接POST</button>
     <div v-for="item in items" :key="item.id">
       <span>{{ item.msg }}</span>
@@ -16,6 +17,7 @@ export default {
   data: function () {
     return {
       items: [],
+      dbName: "test",
     }
   },
   methods: {
@@ -24,7 +26,7 @@ export default {
      */
     addItem: function () {
       this.items.push({
-        key: this.items.length + 1,
+        id: this.items.length + 1,
         msg: "hoge"
       })
     },
@@ -43,6 +45,36 @@ export default {
       const response = await axios.post("http://localhost:3000/api/hoge", body)
       console.log(response)
     },
+    /**
+     * IndexedDBにitemsの内容を保存
+     */
+    saveToIndexedDB: function () {
+      const openRequest = indexedDB.open(this.dbName)
+      const self = this
+      openRequest.onupgradeneeded = function (e) {
+        const db = e.target.result
+        db.createObjectStore(self.dbName, { keyPath: 'id' })
+      }
+      
+      openRequest.onsuccess = async function (e) {
+        const db = e.target.result
+        const transaction = db.transaction(self.dbName, 'readwrite')
+        const store = transaction.objectStore(self.dbName)
+
+        // 本来なら一旦内容を消してからやるべきだけど面倒なので割愛
+
+        const promises = self.items.map(item => {
+          return new Promise(resolve => {
+            const putRequest = store.put(item)
+            putRequest.onsuccess = resolve
+          })
+        })
+        await Promise.all(promises)
+        
+        console.log("Save To IndexedDB OK.")
+        db.close()
+      }
+    }
   }
 }
 </script>
